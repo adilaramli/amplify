ec7e91165bcaf177d721268d7817f727ebc368b9
adilaramli <adilaamnh@gmail.com>
2020-12-29 21:27:36 +0800

    Initial commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:0550d93f-60d3-4bd6-8a87-e5e73fa5f781",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_SEIz7xX1i",
+                        "AppClientId": "34lpgpj6lp5nq8v0uqa4laucmm",
+                        "AppClientSecret": "2s5ecge36psk30gbgt9pid2sh68hu1a91g0j24e0r9arct5rlr1",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
